6963dd0d763a48a3c44e894cb4a142ddee5f8d9a
Yassir Hussain <yasirhussain531@gmail.com>
2022-06-21 20:23:36 +0500

    API Error

--- src/Rocket.js
+++ src/Rocket.js
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/Navbar';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
-import RocketItem from './components/RocketItem';
-
+import RocketItem from "./components/RocketItem";
+import News from "./components/News";
 
 function Rocket() {
   const [rocket, setRocket] = useState([]);
-    useEffect(()=>{
-      const url ='https://api.spacexdata.com/v4/rockets';
-      fetch(url).then(resp1=>resp1.json())
-      .then(resp1=>setRocket(resp1))
-    },[])
-    return (
-      <div className="App">
-            <Navbar/>
-            <img src="https://www.logo.wine/a/logo/SpaceX/SpaceX-White-Dark-Background-Logo.wine.svg" className="d-block mx-auto" style={{width: "350px"}}/>
-            <div>
-        <div className='container my-5'>
-          <h2>SpaceX Rockets</h2> 
-          
-          {
-              rocket.map(
-                (space) =>
-                <RocketItem article_link={space.article_link} launch_date={space.mass.kg} mission_name = {space.name} details = {space.description} flickr_images={space.flickr_images[1]} />
-                )
-              } 
-      </div>
+  const [btnReload, setBtnReload] = useState(false);
+
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v3/rockets")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        setRocket(resp);
+      })
+      .catch(() => {
+        document.getElementById("error").innerHTML =
+          '  We\'re Sorry , " The  API Failed To Load " Please Refresh The Page ';
+        setBtnReload(true);
+      });
+  });
+
+  return (
+    <div className="App">
+      <Navbar />
+      <img
+        src="https://www.logo.wine/a/logo/SpaceX/SpaceX-White-Dark-Background-Logo.wine.svg"
+        className="d-block mx-auto"
+        style={{ width: "350px" }}
+      />
+      <News />
+      {btnReload ? (
+        <button
+          type="button"
+          className="btn btn-primary d-block mx-auto"
+          onClick={() => window.location.reload()}
+        >
+          Refresh Page
+        </button>
+      ) : (
+        " "
+      )}
+      {rocket.map((space) => (
+        <RocketItem
+          article_link={space.article_link}
+          launch_date={space.mass.kg}
+          mission_name={space.name}
+          details={space.description}
+          flickr_images={space.flickr_images[1]}
+        />
+      ))}
     </div>
-  
-      </div>
-    );
+  );
 }
 
 export default Rocket;
